500bb2d291e5499e884f73e7152f528c5c7015a7
suavejavi <suavejavi@gmail.com>
2024-07-04 03:08:00 +0000

    refactor: add deleteQuote method

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -5,12 +5,16 @@ class FirebaseService {
   final CollectionReference quotesCollection = FirebaseFirestore.instance.collection('quotes');
 
   Future<void> addQuote(Quote quote) {
-    return quotesCollection.add(quote.toJson());
+    return quotesCollection.doc(quote.id).set(quote.toJson());
   }
 
   Stream<List<Quote>> getQuotes() {
     return quotesCollection.snapshots().map((snapshot) =>
       snapshot.docs.map((doc) => Quote.fromJson(doc.data() as Map<String, dynamic>)).toList()
     );
+  }
+
+  Future<void> deleteQuote(String id) {
+    return quotesCollection.doc(id).delete();
   }
 }
