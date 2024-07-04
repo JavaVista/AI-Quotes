55f4a1e6c783e2aa21c7e4683ba0a63b492e59e3
suavejavi <suavejavi@gmail.com>
2024-07-04 03:05:35 +0000

    refactor: add connection to gemini firestore function

--- lib/services/gemini_service.dart
+++ lib/services/gemini_service.dart
@@ -1,10 +1,12 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:logger/logger.dart';
 
 class GeminiService {
-  final String geminiUrl = "YOUR_GEMINI_API_URL";
+  final log = Logger();
 
-  Future<String> generateQuote(String category) async {
+  final String geminiUrl = "https://us-central1-ai-quotes-46431.cloudfunctions.net/generateAiQuote"; 
+  Future<Map<String, String>> generateQuote(String category) async {
     final response = await http.post(
       Uri.parse(geminiUrl),
       headers: {'Content-Type': 'application/json'},
@@ -12,9 +14,26 @@ class GeminiService {
     );
 
     if (response.statusCode == 200) {
-      return json.decode(response.body)['quote'];
+      final Map<String, dynamic> data = json.decode(response.body);
+      log.d("Received data Gemini: $data"); // Log the received data
+      return data.cast<String, String>(); // Cast to Map<String, String>
     } else {
+      log.d("Error generating quote Gemini: ${response.statusCode}");
       throw Exception('Failed to generate quote');
     }
   }
 }
+
+// // Example usage:
+// void main() async {
+//   GeminiService geminiService = GeminiService();
+//   try {
+//     Map<String, String> quoteData = await geminiService.generateQuote("love");
+//     log.d("Quote: ${quoteData['quote']}");
+//     log.d("Author: ${quoteData['author']}");
+//     log.d("Category: ${quoteData['category']}");
+//     log.d("Occupation: ${quoteData['occupation']}");
+//   } catch (e) {
+//     log.d("Error: $e");
+//   }
+// }
