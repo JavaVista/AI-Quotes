3f921013754830cf642c0b6a5919dd4132f43242
suavejavi <suavejavi@gmail.com>
2024-07-04 03:03:59 +0000

    refactor: add zenquotes api fetch & cache quotes

--- lib/services/quote_service.dart
+++ lib/services/quote_service.dart
@@ -1,18 +1,66 @@
+import 'dart:async';
 import 'dart:convert';
+import 'dart:math';
 import 'package:http/http.dart' as http;
 import 'package:ai_quotes_app/models/quote.dart';
+import 'package:logger/logger.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class QuoteService {
-  final String apiUrl = "YOUR_QUOTE_API_URL";
+  final String? apiKey = dotenv.env['API_KEY'];
+  final log = Logger();
+  late final String apiUrl;
+  List<Quote> _cachedQuotes = [];
+  Timer? _refreshTimer;
+
+  QuoteService() {
+    apiUrl = "https://zenquotes.io/api/quotes/$apiKey";
+    _fetchAndCacheQuotes();
+    _startRefreshTimer();
+  }
+
+  Future<void> _fetchAndCacheQuotes() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+      if (response.statusCode == 200) {
+        final List<dynamic> quotesJson = json.decode(response.body);
+        _cachedQuotes = quotesJson
+            .map((quoteJson) => Quote(
+                  id: '',
+                  paragraph: quoteJson['q'],
+                  author: quoteJson['a'],
+                  occupation: 'Unknown',
+                ))
+            .toList();
+
+        log.d("Received data ZenQuotes: $_cachedQuotes");
+      } else {
+        log.d("Error fetching quotes ZenQuotes: ${response.statusCode}");
+      }
+    } on Exception catch (e) {
+      log.d("Error fetching quotes ZenQuotes: $e");
+    }
+  }
+
+  void _startRefreshTimer() {
+    _refreshTimer = Timer.periodic(const Duration(hours: 1), (timer) {
+      _fetchAndCacheQuotes();
+    });
+  }
 
   Future<Quote> fetchRandomQuote() async {
-    final response = await http.get(Uri.parse(apiUrl));
-
-    if (response.statusCode == 200) {
-      final quoteJson = json.decode(response.body);
-      return Quote.fromJson(quoteJson);
-    } else {
-      throw Exception('Failed to load quote');
+    if (_cachedQuotes.isEmpty) {
+      await _fetchAndCacheQuotes();
     }
+    if (_cachedQuotes.isNotEmpty) {
+      log.d("Random Quote: $_cachedQuotes");
+      final randomIndex = Random().nextInt(_cachedQuotes.length);
+      return _cachedQuotes[randomIndex];
+    }
+    throw Exception('No quotes available');
+  }
+
+  void dispose() {
+    _refreshTimer?.cancel();
   }
 }
