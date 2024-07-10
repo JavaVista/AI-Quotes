59d41f1dda1fdc3e2790a1655b1f47c7c31f15b6
suavejavi <suavejavi@gmail.com>
2024-07-10 02:50:16 +0000

    feat: :sparkles: add uui package

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -2,16 +2,30 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ai_quotes_app/models/quote.dart';
 
 class FirebaseService {
-  final CollectionReference quotesCollection = FirebaseFirestore.instance.collection('quotes');
+  final CollectionReference quotesCollection =
+      FirebaseFirestore.instance.collection('quotes');
 
   Future<void> addQuote(Quote quote) {
     return quotesCollection.doc(quote.id).set(quote.toJson());
   }
 
+  Future<void> updateQuote(Quote quote) {
+    return quotesCollection.doc(quote.id).update(quote.toJson());
+  }
+
   Stream<List<Quote>> getQuotes() {
-    return quotesCollection.snapshots().map((snapshot) =>
-      snapshot.docs.map((doc) => Quote.fromJson(doc.data() as Map<String, dynamic>)).toList()
-    );
+    return quotesCollection.snapshots().map((snapshot) => snapshot.docs
+        .map((doc) => Quote.fromJson(doc.data() as Map<String, dynamic>))
+        .toList());
+  }
+
+  Stream<List<Quote>> getFavoriteQuotes() {
+    return quotesCollection
+        .where('isFavorite', isEqualTo: true)
+        .snapshots()
+        .map((snapshot) => snapshot.docs
+            .map((doc) => Quote.fromJson(doc.data() as Map<String, dynamic>))
+            .toList());
   }
 
   Future<void> deleteQuote(String id) {
